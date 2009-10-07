8fdc1a7379b04f603c396c8d117bd81d7b0bab08
lvyanfeihappy <lvyanfeihappy@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2009-10-07 11:49:09 +0000

    把CMFT又加回来了。

---
+++ trunk/FlashDBSimDll_Sample/CMFTManager.h
@@ -0,0 +1,28 @@
+#ifndef _CMFT_BUFFER_MANAGER_H_
+#define _CMFT_BUFFER_MANAGER_H_
+
+#include <memory>
+#include <deque>
+#include <hash_map>
+#include "FrameBasedBufferManager.h"
+
+class CMFTManager : public FrameBasedBufferManager
+{
+public:
+	CMFTManager(std::tr1::shared_ptr<class IBlockDevice> pDevice, size_t nPages);
+	virtual ~CMFTManager();
+
+protected:
+	void DoFlush();
+	std::tr1::shared_ptr<struct DataFrame> FindFrame(size_t pageid, bool isWrite);
+	std::tr1::shared_ptr<struct DataFrame> AllocFrame(size_t pageid, bool isWrite);
+
+private:
+
+private:
+	size_t time_;
+	typedef std::deque<std::tr1::shared_ptr<struct CMFTFrame> > StackType;
+	StackType stack_;
+};
+
+#endif
