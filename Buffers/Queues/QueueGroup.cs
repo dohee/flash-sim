76b3ea58f64ca916e1428bdad99505c4f2796f2d
hiyoungcat <hiyoungcat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2009-10-12 13:04:11 +0000

    “半定居”队列写好了，不过尚未测试过正确性。

--- Buffers/Queues/QueueGroup.cs
+++ Buffers/Queues/QueueGroup.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using Buffers.Memory;
 
 namespace Buffers.Queues
 {
 	public abstract class QueueGroup : QueueBase
 	{
+		/// <summary>
+		/// QueueIndex -> IQueue
+		/// </summary>
 		protected List<IQueue> queues = new List<IQueue>();
+		/// <summary>
+		/// QueueIndex -> RouteIndex
+		/// </summary>
+		protected List<uint> offsets = new List<uint>();
+		/// <summary>
+		/// RouteIndex -> (QueueIndex, InnerRootIndex)
+		/// </summary>
 		protected List<Route> routes = new List<Route>();
+
 		private uint curRouteKey, curRouteValue;
+
 
 		protected sealed override void OnCountQueue()
 		{
 			for (int i = 0; i < queues.Count; i++)
 			{
+				offsets.Add((uint)routes.Count);
 				curRouteKey = (uint)i;
 				curRouteValue = 0;
 				queues[i].CountQueue(this.OnInnerCountQueue);
@@ -29,55 +43,61 @@ namespace Buffers.Queues
 		{
 			RoutingNode routing = NATInwards(node);
 			QueueNode returnednode;
-			queues[(int)routing.Major].AccessFrame(routing.InnerNode, out returnednode);
-			newNode = NATOutwards(routing.InnerNode, routing.Via);
+			queues[(int)routing.QueueIndex].AccessFrame(routing.InnerNode, out returnednode);
+			newNode = NATOutwards(routing.QueueIndex, returnednode);
 		}
 
 		public override IFrame Dequeue(QueueNode node)
 		{
 			RoutingNode routing = NATInwards(node);
-			return queues[(int)routing.Major].Dequeue(routing.InnerNode);
+			return queues[(int)routing.QueueIndex].Dequeue(routing.InnerNode);
 		}
 
-		protected RoutingNode NATInwards(QueueNode outer)
+		public override IEnumerator<IFrame> GetEnumerator()
 		{
-			Route route = routes[(int)outer.Index];
-			return new RoutingNode(route.Major, outer.Index, route.Minor, outer.ListNode);
+			foreach (var queue in queues)
+				foreach (var frame in queue)
+					yield return frame;
 		}
-		protected QueueNode NATOutwards(QueueNode inner, uint via)
+
+
+		protected RoutingNode NATInwards(QueueNode outerNode)
 		{
-			uint newmajor = inner.Index - routes[(int)via].Minor + via;
-			return new QueueNode(newmajor, inner.ListNode);
+			Route route = routes[(int)outerNode.Index];
+			return new RoutingNode(route.QueueIndex, route.InnerRouteIndex, outerNode.ListNode);
+		}
+		protected QueueNode NATOutwards(uint queueIndex, QueueNode innerNode)
+		{
+			uint routeIndex = innerNode.Index + offsets[(int)queueIndex];
+			return new QueueNode(routeIndex, innerNode.ListNode);
 		}
 
 
 
 		protected struct Route
 		{
-			public readonly uint Major;
-			public readonly uint Minor;
+			public readonly uint QueueIndex;
+			public readonly uint InnerRouteIndex;
 
-			public Route(uint major, uint minor)
+			public Route(uint queueIndex, uint innerRouteIndex)
 			{
-				Major = major;
-				Minor = minor;
+				QueueIndex = queueIndex;
+				InnerRouteIndex = innerRouteIndex;
 			}
 		}
 
 		protected struct RoutingNode
 		{
-			public readonly uint Major;
-			public readonly uint Via;
+			public readonly uint QueueIndex;
 			public readonly QueueNode InnerNode;
 
-			public RoutingNode(uint major, uint via, uint innerindex,
+			public RoutingNode(uint major, uint innerindex,
 				LinkedListNode<IFrame> innernode)
-				: this(major, via, new QueueNode(innerindex, innernode)) { }
+				: this(major, new QueueNode(innerindex, innernode)) { }
 
-			public RoutingNode(uint major, uint via, QueueNode inner)
+			public RoutingNode(uint major, QueueNode inner)
 			{
-				Major = major;
-				Via = via;
+				QueueIndex = major;
 				InnerNode = inner;
 			}
 		}
