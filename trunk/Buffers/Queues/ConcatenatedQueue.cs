0d753dc2214a68c57ca9c6735a0eba27cd3dba63
hiYoungCat <hiYoungCat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2009-10-12 15:42:05 +0000

    泛化成“首位连接的”队列。

---
+++ trunk/Buffers/Queues/ConcatenatedQueue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Buffers.Memory;
+
+namespace Buffers.Queues
+{
+	public class ConcatenatedQueue : QueueGroup
+	{
+		public ConcatenatedQueue(IQueue front, IQueue back)
+		{
+			queues.Add(front);
+			queues.Add(back);
+		}
+
+		public IQueue FrontQueue { get { return queues[0]; } }
+		public IQueue BackQueue { get { return queues[1]; } }
+
+		public override uint Size
+		{
+			get { return queues[0].Size + queues[1].Size; }
+		}
+
+		public override QueueNode Enqueue(IFrame frame)
+		{
+			QueueNode qn = queues[0].Enqueue(frame);
+			return NATOutwards(0, qn);
+		}
+		public override IFrame Dequeue()
+		{
+			return queues[1].Dequeue();
+		}
+
+		public override void AccessFrame(QueueNode node, out QueueNode newNode)
+		{
+			RoutingNode routing = NATInwards(node);
+			QueueNode returnednode;
+
+			if (routing.QueueIndex == 0)
+				queues[0].AccessFrame(routing.InnerNode, out returnednode);
+			else
+				returnednode = queues[0].Enqueue(queues[1].Dequeue(routing.InnerNode));
+
+			newNode = NATOutwards(0, returnednode);
+		}
+
+		public QueueNode BlowOneFrame()
+		{
+			IFrame victim = queues[0].Dequeue();
+			QueueNode qn = queues[1].Enqueue(victim);
+			return NATOutwards(1, qn);
+		}
+	}
+}
