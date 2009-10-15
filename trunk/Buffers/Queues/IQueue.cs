30e96a54325b9cdc9df33104fb0f35c17f77b4ae
hiYoungCat <hiYoungCat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2009-10-15 11:05:07 +0000

    换一种方法来生成路由。

---
+++ trunk/Buffers/Queues/IQueue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Buffers.Memory;
+
+namespace Buffers.Queues
+{
+	public struct QueueNode
+	{
+		public readonly uint Index;
+		public readonly LinkedListNode<IFrame> ListNode;
+
+		public QueueNode(LinkedListNode<IFrame> node)
+			: this(0, node) { }
+
+		public QueueNode(uint major, LinkedListNode<IFrame> node)
+		{
+			Index = major;
+			ListNode = node;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null)
+				return false;
+			if (this.GetType() != obj.GetType())
+				return false;
+
+			return this == (QueueNode)obj;
+		}
+
+		public static bool operator ==(QueueNode left, QueueNode right)
+		{
+			return left.Index == right.Index && left.ListNode == right.ListNode;
+		}
+
+		public override int GetHashCode()
+		{
+			return Index.GetHashCode() ^ ListNode.GetHashCode();
+		}
+	}
+
+
+	public interface IQueue : IEnumerable<IFrame>
+	{
+		uint Size { get; }
+		uint BasicQueueCount { get; }
+
+		QueueNode Enqueue(IFrame frame);
+		IFrame Dequeue();
+		IFrame Dequeue(QueueNode node);
+		QueueNode AccessFrame(QueueNode node);
+	}
+}
