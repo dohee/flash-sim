acdbfba710975fa1b4d558c024df95f422e583f6
hiYoungCat <hiYoungCat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2009-10-19 10:04:33 +0000

    整理代码文件。

--- Buffers/Memory/Frame.cs
+++ Buffers/Memory/Frame.cs
@@ -8,8 +8,8 @@ namespace Buffers.Memory
 	public class Frame : IFrame
 	{
 		protected uint id;
-        protected int slotid;
-        protected bool dirty = false;
+		protected int slotid;
+		protected bool dirty = false;
 
 		public uint Id { get { return id; } set { id = value; } }
 		public bool Dirty { get { return dirty; } set { dirty = value; } }
@@ -25,46 +25,4 @@ namespace Buffers.Memory
 			this.slotid = slotid;
 		}
 	}
-
-//This is the frame for inter reference recency
-    public class IRRFrame : Frame
-    {
-        public uint readIRR;
-        public uint writeIRR;
-        public uint readRecency;
-        public uint writeRecency;
-
-        public IRRFrame(uint id)
-            : this(id, -1) { }
-
-        public IRRFrame(uint id, int slotid)
-            :base(id, slotid)
-		{
-            readIRR = 0;
-            writeIRR = 0;
-		}
-
-        //get the power of this page for evict selection
-        public double getPower()
-        {
-            double power = 0;
-            double aveReadIRR = ((double)readIRR+readRecency)/2;
-            double aveWriteIRR = ((double)writeIRR+writeRecency)/2;
-
-            if (readIRR == 0) aveReadIRR = readRecency; //only read once
-            if (writeIRR == 0) aveWriteIRR = writeRecency;  //only write once
-
-            if (aveReadIRR != 0)        //0 means this page has not been read before.
-            {
-                power += (double)Config.ReadCost / aveReadIRR;
-            }
-            if (aveWriteIRR != 0)
-            {
-                power += (double)Config.WriteCost / aveWriteIRR;
-            }
-            return power;
-        }
-
-    }
-
 }
