d91cba5c1e4f35a5eac05a040318f4b0741f59c8
hiyoungcat <hiyoungcat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2009-11-26 12:57:16 +0000

    未成品。

---
+++ ParseStrace/IOItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParseStrace
+{
+	struct IOItem
+	{
+		public readonly string Filename;
+		public readonly bool IsWrite;
+		public readonly long Position;
+		public readonly long Length;
+
+		public IOItem(string filename, bool isWrite, long pos, long length)
+		{
+			Filename = filename;
+			IsWrite = isWrite;
+			Position = pos;
+			Length = length;
+		}
+	}
+
+}
--- ParseStrace/Program.cs
+++ ParseStrace/Program.cs
@@ -11,8 +11,6 @@ namespace ParseStrace
 	{
 		static Regex regexLine = new Regex(@"(\w+)\((.+)\)\s+= ((0x[\dabcdefABCDEF]+)|(-?\d+))");
 		static Regex regexPid = new Regex(@"^(\d+)\s");
-		static Regex regexOpen = new Regex(@"^\""(.+)\"",");
-		static Regex regexFirstArg = new Regex(@"^(\d+),");
 
 
 		static void Main(string[] args)
@@ -67,119 +65,5 @@ namespace ParseStrace
 
 
 
-		struct IOItem
-		{
-			public readonly string Filename;
-			public readonly bool IsWrite;
-			public readonly long Position;
-			public readonly long Length;
-
-			public IOItem(string filename, bool isWrite, long pos, long length)
-			{
-				Filename = filename;
-				IsWrite = isWrite;
-				Position = pos;
-				Length = length;
-			}
-		}
-
-		struct FileDesc
-		{
-			public readonly int PID;
-			public readonly int FD;
-
-			public FileDesc(int pid, int fd)
-			{
-				PID = pid;
-				FD = fd;
-			}
-
-			//TODO eq
-		}
-
-		class FileState
-		{
-			public readonly string Filename;
-			public long Position = 0;
-
-			public FileState(string filename)
-			{
-				Filename = filename;
-			}
-		}
-
-
-		static List<IOItem> records = new List<IOItem>();
-		static Dictionary<FileDesc, FileState> curFiles = new Dictionary<FileDesc, FileState>();
-
-		static void OnOpen(int pid, string args, long ret)
-		{
-			FileDesc fd = new FileDesc(pid, (int)ret);
-			string filename = regexOpen.Match(args).Groups[1].Value;
-			Debug.Assert(!curFiles.ContainsKey(fd));
-			curFiles[fd] = new FileState(filename);
-		}
-		static void OnClose(int pid, string args)
-		{
-			FileDesc fd = new FileDesc(pid, int.Parse(args));
-			curFiles.Remove(fd);
-		}
-		static void OnReadWrite(bool isWrite, int pid, string args, long ret)
-		{
-			int fdnum = int.Parse(regexFirstArg.Match(args).Groups[1].Value);
-			if (fdnum < 3)
-				return;
-
-			FileDesc fd = new FileDesc(pid, fdnum);
-			FileState fs;
-
-			if (!curFiles.TryGetValue(fd, out fs))
-			{
-				fs = new FileState(fd.ToString());
-				curFiles[fd] = fs;
-			}
-
-			records.Add(new IOItem(fs.Filename, isWrite, fs.Position, ret));
-			fs.Position += ret;
-		}
-		static void OnLSeek(int pid, string args, long ret)
-		{
-			int fdnum = int.Parse(regexFirstArg.Match(args).Groups[1].Value);
-			FileDesc fd = new FileDesc(pid, fdnum);
-			Debug.Assert(curFiles.ContainsKey(fd));
-
-			FileState fs = curFiles[fd];
-			fs.Position = ret;
-		}
-		static void OnLLSeek(int pid, string args, long ret)
-		{
-			Debug.Assert(false);
-		}
-
-
-		static void Output(TextWriter writer)
-		{
-			//TODO fd
-			Dictionary<string, int> fileIndex = new Dictionary<string, int>();
-			const int kPagesize = 4096;
-
-			foreach (IOItem item in records)
-			{
-				int index;
-				if (!fileIndex.TryGetValue(item.Filename, out index))
-				{
-					index = fileIndex.Count;
-					fileIndex[item.Filename] = index;
-				}
-
-				Debug.Assert(index < kPagesize);
-				int pos = (int)(item.Position / kPagesize);
-				int pos2 = (int)((item.Position + item.Length) / kPagesize);
-				int len = pos2 - pos + 1;
-				pos += index * (int)(0x80000000L / kPagesize);
-
-				writer.WriteLine("{0}\t{1}\t{2}", pos, len, item.IsWrite ? 1 : 0);
-			}
-		}
 	}
 }
