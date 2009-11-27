b71b6772fd211d280be173930384496495c12f39
hiYoungCat <hiYoungCat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2009-11-27 01:54:00 +0000

    临时提交。

--- trunk/ParseStrace/ProcFDTable.cs
+++ trunk/ParseStrace/ProcFDTable.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -11,15 +12,31 @@ namespace ParseStrace
 		private static Regex regexOpen = new Regex(@"^\""(.+)\"",");
 		private static Regex regexFirstArg = new Regex(@"^(\d+),");
 
+		private TextWriter writer;
 		private List<IOItem> records = new List<IOItem>();
 		private Dictionary<int, FileState> curFiles = new Dictionary<int, FileState>();
+
+
+		public ProcFDTable(TextWriter writer)
+		{
+			this.writer = writer;
+		}
+
+		public ProcFDTable Fork()
+		{
+			ProcFDTable other = new ProcFDTable(writer);
+
+			foreach (var item in curFiles)
+				other.curFiles.Add(item.Key, item.Value);
+
+			return other;
+		}
 
 
 		public void OnOpen(string args, long ret)
 		{
 			int fd = (int)ret;
 			string filename = regexOpen.Match(args).Groups[1].Value;
-			Debug.Assert(!curFiles.ContainsKey(fd));
 			curFiles[fd] = new FileState(filename);
 		}
 
@@ -35,16 +52,20 @@ namespace ParseStrace
 			if (fd < 3)
 				return;
 
-			FileState fs;
+			Debug.Assert(curFiles.ContainsKey(fd));
+			FileState fs = curFiles[fd];
+			/*FileState fs;
 
 			if (!curFiles.TryGetValue(fd, out fs))
 			{
 				fs = new FileState(fd.ToString());
 				curFiles[fd] = fs;
-			}
+			}*/
 
-			records.Add(new IOItem(fs.Filename, isWrite, fs.Position, ret));
+			IOItem item = new IOItem(fs.Filename, isWrite, fs.Position, ret);
+			records.Add(item);
 			fs.Position += ret;
+			Output(item);
 		}
 
 		public void OnLSeek(string args, long ret)
@@ -56,35 +77,12 @@ namespace ParseStrace
 			fs.Position = ret;
 		}
 
-		public void OnLLSeek(string args, long ret)
+
+		private void Output(IOItem item)
 		{
-			Debug.Assert(false);
-		}
-
-
-		public void Output(TextWriter writer)
-		{
-			//TODO fd
-			Dictionary<string, int> fileIndex = new Dictionary<string, int>();
-			const int kPagesize = 4096;
-
-			foreach (IOItem item in records)
-			{
-				int index;
-				if (!fileIndex.TryGetValue(item.Filename, out index))
-				{
-					index = fileIndex.Count;
-					fileIndex[item.Filename] = index;
-				}
-
-				Debug.Assert(index < kPagesize);
-				int pos = (int)(item.Position / kPagesize);
-				int pos2 = (int)((item.Position + item.Length) / kPagesize);
-				int len = pos2 - pos + 1;
-				pos += index * (int)(0x80000000L / kPagesize);
-
-				writer.WriteLine("{0}\t{1}\t{2}", pos, len, item.IsWrite ? 1 : 0);
-			}
+			writer.WriteLine("{0}\t{1}\t{2}\t{3}",
+				item.Position, item.Length,
+				item.IsWrite ? 1 : 0, item.Filename);
 		}
 
 
