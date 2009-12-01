5aff9aa2a69c7c027ab0f957d5737c00b10871c1
hiyoungcat <hiyoungcat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2009-12-01 09:46:59 +0000

    又做好了几个 syscall。 只差 unfinished.

--- ParseStrace/IOItem.cs
+++ ParseStrace/IOItem.cs
@@ -4,23 +4,34 @@ using System.Text;
 
 namespace ParseStrace
 {
-	struct IOItem
+	enum FDType : byte
 	{
+		File,
+		Terminal,
+		Pipe,
+	}
+
+	class IOItem
+	{
+		public readonly int Pid;
 		public readonly string Filename;
-		public readonly short FD;
-		public readonly bool IsTerminal;
+		public readonly short FDNum;
+		public readonly FDType FDType;
 		public readonly bool IsWrite;
 		public readonly long Position;
 		public readonly long Length;
 
-		public IOItem(string filename, short fd, bool isWrite, long pos, long length)
-			: this(filename, fd, isWrite, pos, length, false) { }
+		public IOItem(int pid, string filename, short fd,
+			bool isWrite, long pos, long length)
+			: this(pid, filename, fd, isWrite, pos, length, FDType.File) { }
 
-		public IOItem(string filename, short fd, bool isWrite, long pos, long length, bool isTerminal)
+		public IOItem(int pid, string filename, short fd,
+			bool isWrite, long pos, long length, FDType type)
 		{
+			Pid = pid;
 			Filename = filename;
-			FD = fd;
-			IsTerminal = isTerminal;
+			FDNum = fd;
+			FDType = type;
 			IsWrite = isWrite;
 			Position = pos;
 			Length = length;
@@ -31,8 +42,22 @@ namespace ParseStrace
 			return string.Format(
 				"IOItem[{0} Len={1} at Pos={2} on {3} via FD={4}]",
 				IsWrite ? "write" : "read", Length,
-				Position, Filename, FD);
+				Position, Filename, FDNum);
 		}
-	}
 
+		public string TypeString
+		{
+			get
+			{
+				switch (FDType)
+				{
+					case FDType.File: return "File";
+					case FDType.Terminal: return "Term";
+					case FDType.Pipe: return "Pipe";
+					default: return null;
+				}
+			}
+		}
+
+	}
 }
