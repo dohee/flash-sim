6519000f59a1a8ebef39533083f36820b3f35fc9
hiyoungcat <hiyoungcat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2009-12-03 03:48:03 +0000

    two-phase 实现，避免过大内存占用。

--- ParseStrace/Program.cs
+++ ParseStrace/Program.cs
@@ -10,8 +10,7 @@ namespace ParseStrace
 {
 	class Program
 	{
-		static Regex regexLine = new Regex(@"(\w+)\((.*)\)\s+= ((0x[\dabcdefABCDEF]+)|(-?\d+))");
-		static Regex regexPid = new Regex(@"^(\d+)\s");
+		static Regex regexLine = new Regex(@"(\w+)\((.*)\)\s+= ([^ ]+)");
 		static Regex regexResumed = new Regex(@"<\.\.\. (\w+) resumed> (.+)$");
 
 		static Dictionary<int, ProcFDTable> fdTables = new Dictionary<int, ProcFDTable>();
@@ -20,11 +19,23 @@ namespace ParseStrace
 
 		static void Main(string[] args)
 		{
-			string filename, line;
-			TextReader reader = GetReader(args, out filename);
+			string filename = args[0];
+			FileStream fileStream = new FileStream(filename, FileMode.Open);
+
 			Console.WriteLine("# Parsed Strace: {0}", filename);
 			Console.WriteLine("# Date: {0}", DateTime.Now);
 			storage = new IOItemStorageVerbose(Console.Out);
+
+			StreamReader reader = new StreamReader(fileStream);
+			ProceedFile(reader, 1);
+			storage.PhaseBetween();
+			fileStream.Seek(0, SeekOrigin.Begin);
+			ProceedFile(reader, 2);
+		}
+
+		static void ProceedFile(TextReader reader, int phase)
+		{
+			string line;
 			int count = 0;
 
 			while ((line = reader.ReadLine()) != null)
@@ -32,9 +43,8 @@ namespace ParseStrace
 				if (++count % 10000 == 0)
 					Console.Error.WriteLine(count);
 
-				Match mpid = regexPid.Match(line);
-				int pid = 0;
-				int.TryParse(mpid.Groups[1].Value, out pid);
+				int firstSpace = line.IndexOf(' ');
+				int pid = int.Parse(line.Substring(0, firstSpace));
 
 				Match mresumed;
 
@@ -43,48 +53,34 @@ namespace ParseStrace
 					string former = line.Substring(0, line.Length - 16);
 					fdTables[pid].OnUnfinished(former);
 				}
-				else if ((mresumed = regexResumed.Match(line)).Success)
+				else if (line.Contains("resumed> ") && (mresumed = regexResumed.Match(line)).Success)
 				{
 					string former = fdTables[pid].OnResumed();
 					string latter = mresumed.Groups[2].Value;
 					string whole = former + latter;
-					ProceedLine(pid, whole);
+					ProceedLine(pid, whole, phase);
 				}
 				else
 				{
-					ProceedLine(pid, line);
+					ProceedLine(pid, line, phase);
 				}
 			}
-
-			storage.Output();
 		}
 
 
-		static TextReader GetReader(string[] args, out string filename)
-		{
-			if (args.Length >= 1)
-			{
-				filename = args[0];
-				return new StreamReader(args[0]);
-			}
-			else
-			{
-				filename = "-";
-				return Console.In;
-			}
-		}
-
-
-		static void ProceedLine(int pid, string line)
+		static void ProceedLine(int pid, string line, int phase)
 		{
 			Match m = regexLine.Match(line);
 			if (!m.Success)
 				return;
 
-			long ret;
-			if (!long.TryParse(m.Groups[3].Value, out ret))
-				ret = long.Parse(m.Groups[3].Value.Substring(2), NumberStyles.HexNumber);
+			string retstring = m.Groups[3].Value;
+			if (retstring == "?")
+				return;
 
+			long ret;
+			if (!long.TryParse(retstring, out ret))
+				ret = long.Parse(retstring.Substring(2), NumberStyles.HexNumber);
 			if (ret < 0)
 				return;
 
@@ -115,8 +111,8 @@ namespace ParseStrace
 				case "fcntl": table.OnFcntl(args, ret); break;
 				case "lseek": table.OnLSeek(args, ret); break;
 				case "pipe": table.OnPipe(args); break;
-				case "read": table.OnReadWrite(false, args, ret); break;
-				case "write": table.OnReadWrite(true, args, ret); break;
+				case "read": table.OnReadWrite(false, args, ret, phase); break;
+				case "write": table.OnReadWrite(true, args, ret, phase); break;
 
 				default: break;
 			}
