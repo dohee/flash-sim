c755711e26bb017c58ded8acf48a805cc563cbee
hiYoungCat <hiYoungCat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2009-12-03 09:02:05 +0000

    小改动。

--- trunk/Buffers/Program.cs
+++ trunk/Buffers/Program.cs
@@ -20,16 +20,16 @@ namespace Buffers
 			ManagerGroup group = new ManagerGroup();
 
 			group.Add(new LRU(npages));
-			group.Add(Wrapper.CreateCFLRU(npages, npages / 2));
+			//group.Add(Wrapper.CreateCFLRU(npages, npages / 2));
 			//group.Add(Wrapper.CreateCFLRUD(npages));
 			//group.Add(Wrapper.CreateLRUWSR(npages));
 			//group.Add(new Tn(npages, ratio, new TnConfig(false, false, 0, 0, false)));
 			//group.Add(new Tn(npages, ratio, new TnConfig(false, true, 0, 0, false)));
 			//group.Add(new Tn(npages, ratio, new TnConfig(true, false, 0, 0, false)));
 			//group.Add(new Tn(npages, ratio, new TnConfig(true, true, 0, 0, false)));
-			//group.Add(new Tn(npages, ratio, new TnConfig(true, false, npages / 4, npages / 2, false)));
+			group.Add(new Tn(npages, ratio, new TnConfig(true, false, npages / 4, npages / 2, false)));
 			//group.Add(new Tn(npages, ratio, new TnConfig(true, false, npages / 4, 0, true)));
-			group.Add(new CMFTByCat(npages));
+			//group.Add(new CMFTByCat(npages));
 			//group.Add(new OldBlowerByCat(npages));
 			//group.Add(new BlowerByCat(npages));
 			//group.Add(new BlowerByLyf(npages));
