b27386862d90b66572f326989b73f68817120b92
hiyoungcat <hiyoungcat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2010-01-18 10:44:52 +0000

    从参数获取更多信息。

--- trunk/Buffers/Exceptions.cs
+++ trunk/Buffers/Exceptions.cs
@@ -14,4 +14,15 @@ namespace Buffers
 		protected DataNotConsistentException(SerializationInfo info, StreamingContext context)
 			: base(info, context) { }
 	}
+
+	[Serializable]
+	public class InvalidCmdLineArgumentException : ApplicationException
+	{
+		public InvalidCmdLineArgumentException() { }
+		public InvalidCmdLineArgumentException(string message) : base(message) { }
+		public InvalidCmdLineArgumentException(string message, Exception inner)
+			: base(message, inner) { }
+		protected InvalidCmdLineArgumentException(SerializationInfo info, StreamingContext context)
+			: base(info, context) { }
+	}
 }
