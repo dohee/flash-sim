4df5ecfe0a005f4e31b1213c7bc090306ea02e35
hiyoungcat <hiyoungcat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2010-01-24 09:41:01 +0000

    新版 Tn 完成。

--- trunk/Buffers/Managers/Tn.cs
+++ trunk/Buffers/Managers/Tn.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Buffers.Lists;
 using Buffers.Memory;
-using Buffers.Queues;
 
 namespace Buffers.Managers
 {
@@ -30,12 +30,11 @@ namespace Buffers.Managers
 		}
 	}
 
-
-#pragma warning disable 0618
-	public sealed class Tn : FrameBasedManager
-#pragma warning restore 0618
+	
+	public sealed class Tn : BufferManagerBase
 	{
-		private readonly MultiConcatLRUQueue<IFrame> q;
+		private readonly MultiList<IFrame> q = new MultiList<IFrame>(6);
+		private readonly Dictionary<uint, MultiListNode<IFrame>> map = new Dictionary<uint, MultiListNode<IFrame>>();
 
 		private readonly TnConfig conf;
 		private readonly float kickn;
@@ -55,14 +54,9 @@ namespace Buffers.Managers
 		public Tn(IBlockDevice dev, uint npages, float kickN, TnConfig conf)
 			: base(dev, npages)
 		{
-			q = new MultiConcatLRUQueue<IFrame>(new ConcatenatedLRUQueue<IFrame>[] {
-				new ConcatenatedLRUQueue<IFrame>(
-					new FIFOQueue<IFrame>(), new FIFOQueue<IFrame>()),
-				new ConcatenatedLRUQueue<IFrame>(
-					new FIFOQueue<IFrame>(), new FIFOQueue<IFrame>()),
-				new ConcatenatedLRUQueue<IFrame>(
-					new FIFOQueue<IFrame>(), new FIFOQueue<IFrame>())
-			});
+			q.SetConcat(0, 1);
+			q.SetConcat(2, 3);
+			q.SetConcat(4, 5);
 
 			if (conf.CNRLimitRatio == 0.0)
 				conf.CNRLimitRatio = 0.5f;
@@ -106,145 +100,135 @@ namespace Buffers.Managers
 			cr = Math.Max(cr, 0);
 			cr = Math.Min(cr, pool.NPages - SRLimit);
 			crlimit_ = cr;
-			//Console.WriteLine("CurrentCRLimit: " + cr);
 		}
 
 
-		protected override QueueNode<IFrame> OnHit(QueueNode<IFrame> node, AccessType type)
+		protected override void DoAccess(uint pageid, byte[] resultOrData, AccessType type)
 		{
-			bool isRead = (type == AccessType.Read);
-			bool isWrite = (type == AccessType.Write);
-			bool resident = node.ListNode.Value.Resident;
-			uint inwhichqueue = q.GetRoute(node);
-			bool inClean = (inwhichqueue == 0);
-			bool inDirty = (inwhichqueue == 1);
-			bool inSingle = (inwhichqueue == 2);
+			MultiListNode<IFrame> node;
+			IFrame f;
 
-			if (inClean && isRead)
+			if (!map.TryGetValue(pageid, out node))
 			{
-				node = q.Access(node);
-				if (!resident)
-				{
+				f = new Frame(pageid);
+				PerformAccess(f, resultOrData, type);
+
+				map[pageid] = q.AddFirst(
+					SRLimit != 0 ? 4 : type == AccessType.Read ? 0 : 2, f);
+
+				return;
+			}
+
+			f = node.Value;
+			int inwhichqueue = node.ListIndex;
+			bool inCR = (inwhichqueue == 0);
+			bool inCNR = (inwhichqueue == 1);
+			bool inDR = (inwhichqueue == 2);
+			bool inDNR = (inwhichqueue == 3);
+			bool inSR = (inwhichqueue == 4);
+			bool inSNR = (inwhichqueue == 5);
+			bool inClean = inCR || inCNR;
+			bool inDirty = inDR || inDNR;
+			bool inSingle = inSR || inSNR;
+
+			if (inClean && type == AccessType.Read)
+			{
+				node = q.AddFirst(0, q.Remove(node));
+				if (!f.Resident)
 					EnlargeCRLimit(1);
-					IFrame f = node.ListNode.Value;
-					f.DataSlotId = pool.AllocSlot();
-					dev.Read(f.Id, pool[f.DataSlotId]);
-				}
-				return node;
+				PerformAccess(f, resultOrData, type);
 			}
-			else if (inClean && isWrite)
+			else if (inClean && type == AccessType.Write)
 			{
-				IFrame f = q.Dequeue(node);
-				if (!resident)
-					f.DataSlotId = pool.AllocSlot();
-				return q.Enqueue(1, f);
+				q.Remove(node);
+				PerformAccess(f, resultOrData, type);
+				node = q.AddFirst(2, f);
 			}
-			else if (inDirty && isRead && resident)
+			else if (inDR && type == AccessType.Read)
 			{
-                //Random rand=new Random();
 				if (conf.AdjustDRWhenReadInDR)
-					return q.Access(node);
-				else
-					return node;
+					node = q.AddFirst(2, q.Remove(node));
+				PerformAccess(f, resultOrData, type);
 			}
-			else if (inDirty && isRead && !resident)
+			else if (inDNR && type == AccessType.Read)
 			{
 				if (conf.EnlargeCRWhenReadInDNR)
 					EnlargeCRLimit(1);
-				IFrame f = q.Dequeue(node);
-				f.DataSlotId = pool.AllocSlot();
-				dev.Read(f.Id, pool[f.DataSlotId]);
-				return q.Enqueue(0, f);
+				q.Remove(node);
+				PerformAccess(f, resultOrData, type);
+				node = q.AddFirst(0, f);
 			}
-			else if (inDirty && isWrite)
+			else if (inDirty && type == AccessType.Write)
 			{
-				node = q.Access(node);
-				if (!resident)
-				{
+				q.Remove(node);
+				if (!f.Resident)
 					EnlargeCRLimit(-kickn);
-					node.ListNode.Value.DataSlotId = pool.AllocSlot();
-				}
-				return node;
+				PerformAccess(f, resultOrData, type);
+				node = q.AddFirst(2, f);
 			}
-			else if (inSingle && resident)
+			else if (inSR)
 			{
 				if (conf.PickOffSRWhenHitInSR)
-					return q.Enqueue((isRead ? 0 : 1), q.Dequeue(node));
-				else
-					return node;
+					node = q.AddFirst((type == AccessType.Read ? 0 : 2), q.Remove(node));
+				PerformAccess(f, resultOrData, type);
 			}
-			else if (inSingle && !resident)
+			else if (inSNR)
 			{
-				IFrame f = q.Dequeue(node);
-				f.DataSlotId = pool.AllocSlot();
-				if (isRead)
-					dev.Read(f.Id, pool[f.DataSlotId]);
-				return q.Enqueue((isRead ? 0 : 1), f);
+				q.Remove(node);
+				PerformAccess(f, resultOrData, type);
+				node = q.AddFirst(type == AccessType.Read ? 0 : 2, f);
 			}
 			else
 			{
 				throw new Exception("Should not come here.");
 			}
-		}
 
-		protected override QueueNode<IFrame> OnMiss(IFrame allocatedFrame, AccessType type)
-		{
-			if (SRLimit == 0)
-			{
-				if (type == AccessType.Read)
-					return q.Enqueue(0, allocatedFrame);
-				else
-					return q.Enqueue(1, allocatedFrame);
-			}
-			else
-			{
-				return q.Enqueue(2, allocatedFrame);
-			}
+			map[pageid] = node;
 		}
 
 		protected override void OnPoolFull()
 		{
-			QueueNode<IFrame> qn;
-			if (q.GetFrontSize(0) > CRLimit) qn = q.BlowOneItem(0);
-			else if (q.GetFrontSize(1) > DRLimit) qn = q.BlowOneItem(1);
-			else if (q.GetFrontSize(2) > SRLimit) qn = q.BlowOneItem(2);
-			else if (q.GetFrontSize(0) != 0) qn = q.BlowOneItem(0);
-			else if (q.GetFrontSize(1) != 0) qn = q.BlowOneItem(1);
-			else qn = q.BlowOneItem(2);
+			MultiListNode<IFrame> node;
+			if (q.GetNodeCount(0) > CRLimit) node = q.Blow(0);
+			else if (q.GetNodeCount(2) > DRLimit) node = q.Blow(2);
+			else if (q.GetNodeCount(4) > SRLimit) node = q.Blow(4);
+			else if (q.GetNodeCount(0) != 0) node = q.Blow(0);
+			else if (q.GetNodeCount(2) != 0) node = q.Blow(2);
+			else node = q.Blow(4);
 
-			IFrame f = qn.ListNode.Value;
+			IFrame f = node.Value;
 			WriteIfDirty(f);
 			pool.FreeSlot(f.DataSlotId);
 			f.DataSlotId = -1;
-			map[f.Id] = qn;
+			map[f.Id] = node;
 
-			if (q.GetBackSize(0) > CNRLimit)
-				map.Remove(q.Dequeue(0).Id);
-			if (q.GetBackSize(1) > DNRLimit)
-				map.Remove(q.Dequeue(1).Id);
-			if (q.GetBackSize(2) > SNRLimit)
-				map.Remove(q.Dequeue(2).Id);
+			if (q.GetNodeCount(1) > CNRLimit)
+				map.Remove(q.RemoveLast(1).Id);
+			if (q.GetNodeCount(3) > DNRLimit)
+				map.Remove(q.RemoveLast(3).Id);
+			if (q.GetNodeCount(5) > SNRLimit)
+				map.Remove(q.RemoveLast(5).Id);
 		}
 
 		protected override void DoFlush()
 		{
 			var drpages = new List<uint>();
 
-			foreach (var entry in map)
+			foreach (MultiListNode<IFrame> node in map.Values)
 			{
-				IFrame f = entry.Value.ListNode.Value;
+				IFrame f = node.Value;
 				if (!f.Dirty)
 					continue;
 
 				dev.Write(f.Id, pool[f.DataSlotId]);
 				f.Dirty = false;
 
-				if (q.GetRoute(entry.Value) == 1)
-					drpages.Add(entry.Key);
+				if (node.ListIndex == 2 || node.ListIndex == 3)
+					drpages.Add(f.Id);
 			}
 
-			foreach (var pageid in drpages)
-				map[pageid] = q.Enqueue(0, q.Dequeue(map[pageid]));
+			foreach (uint pageid in drpages)
+				map[pageid] = q.AddFirst(0, q.Remove(map[pageid]));
 		}
 	}
 }
