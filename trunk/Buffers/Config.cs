02b6320451db844bd843e348dd9c68d0a130f229
hiYoungCat <hiYoungCat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2010-03-02 15:33:12 +0000

    排列算法。

--- trunk/Buffers/Config.cs
+++ trunk/Buffers/Config.cs
@@ -51,46 +51,22 @@ namespace Buffers
            
 #pragma warning disable 0169 // "Never used"
 #pragma warning disable 0618 // "Obsolete"
-		[ManagerFactory("Trival")]
-		static IBufferManager CreateTrival(uint npages, string[] args, bool verify)
-		{
-			return new TrivalManager(CreateDevice(verify));
-		}
-		[ManagerFactory("CFLRU")]
-		static IBufferManager CreateCFLRU(uint npages, string[] args, bool verify)
-		{
-			return new CFLRU(CreateDevice(verify), npages, float.Parse(args[0]));
-		}
-		[ManagerFactory("LRUWSR")]
-		static IBufferManager CreateLRUWSR(uint npages, string[] args, bool verify)
-		{
-			return new LRUWSR(CreateDevice(verify), npages);
-		}
+		// 注意：按字母表顺序排列
         [ManagerFactory("CCFLRU")]
         static IBufferManager CreateCCFLRU(uint npages, string[] args, bool verify)
         {
             return new CCFLRU(CreateDevice(verify), npages);
         } 
+		[ManagerFactory("CFLRU")]
+		static IBufferManager CreateCFLRU(uint npages, string[] args, bool verify)
+		{
+			return new CFLRU(CreateDevice(verify), npages, float.Parse(args[0]));
+		}
 		[ManagerFactory("CMFT")]
 		static IBufferManager CreateCMFT(uint npages, string[] args, bool verify)
 		{
 			return new CMFTByCat(CreateDevice(verify), npages);
 		}
-		[ManagerFactory("LRU")]
-		static IBufferManager CreateLRU(uint npages, string[] args, bool verify)
-		{
-			return new LRU(CreateDevice(verify), npages);
-		}
-        [ManagerFactory("LIRS")]
-        static IBufferManager CreateLIRS(uint npages, string[] args, bool verify)
-        {
-            return new LIRS_ByWu(CreateDevice(verify), npages, float.Parse(args[0]));
-        }
-        [ManagerFactory("FLRUByLyf")]
-        static IBufferManager CreateFLRUByLyf(uint npages, string[] args, bool verify)
-        {
-            return new FLRU(CreateDevice(verify), npages);
-        }
 		[ManagerFactory("FLIRSByCat")]
 		static IBufferManager CreateFLIRSByCat(uint npages, string[] args, bool verify)
 		{
@@ -101,22 +77,26 @@ namespace Buffers
 		{
 			return new FLIRSbyLyf2(CreateDevice(verify), npages, double.Parse(args[0]));
 		}
-		[ManagerFactory("Tn")]
-		[ManagerFactory("ACAR")]
-		static IBufferManager CreateTn(uint npages, string[] args, bool verify)
+		[ManagerFactory("FLRU")]
+        [ManagerFactory("FLRUByLyf")]
+        static IBufferManager CreateFLRUByLyf(uint npages, string[] args, bool verify)
+        {
+            return new FLRU(CreateDevice(verify), npages);
+        }
+        [ManagerFactory("LIRS")]
+        static IBufferManager CreateLIRS(uint npages, string[] args, bool verify)
+        {
+            return new LIRS_ByWu(CreateDevice(verify), npages, float.Parse(args[0]));
+        }
+		[ManagerFactory("LRU")]
+		static IBufferManager CreateLRU(uint npages, string[] args, bool verify)
 		{
-			if (args.Length == 0)
-				return new Tn(CreateDevice(verify), npages, Ratio);
-			else
-				return new Tn(CreateDevice(verify), npages, Ratio, new TnConfig(
-					int.Parse(args[0]) != 0,
-					int.Parse(args[1]) != 0,
-					int.Parse(args[2]) != 0,
-					float.Parse(args[3]),
-					float.Parse(args[4]),
-					float.Parse(args[5]),
-					float.Parse(args[6])
-					));
+			return new LRU(CreateDevice(verify), npages);
+		}
+		[ManagerFactory("LRUWSR")]
+		static IBufferManager CreateLRUWSR(uint npages, string[] args, bool verify)
+		{
+			return new LRUWSR(CreateDevice(verify), npages);
 		}
 		[ManagerFactory("OldTn")]
 		static IBufferManager CreateOldTn(uint npages, string[] args, bool verify)
@@ -147,6 +127,28 @@ namespace Buffers
 					uint.Parse(args[6]),
 					int.Parse(args[2]) != 0
 					));
+		}
+		[ManagerFactory("Tn")]
+		[ManagerFactory("ACAR")]
+		static IBufferManager CreateTn(uint npages, string[] args, bool verify)
+		{
+			if (args.Length == 0)
+				return new Tn(CreateDevice(verify), npages, Ratio);
+			else
+				return new Tn(CreateDevice(verify), npages, Ratio, new TnConfig(
+					int.Parse(args[0]) != 0,
+					int.Parse(args[1]) != 0,
+					int.Parse(args[2]) != 0,
+					float.Parse(args[3]),
+					float.Parse(args[4]),
+					float.Parse(args[5]),
+					float.Parse(args[6])
+					));
+		}
+		[ManagerFactory("Trival")]
+		static IBufferManager CreateTrival(uint npages, string[] args, bool verify)
+		{
+			return new TrivalManager(CreateDevice(verify));
 		}
 #pragma warning restore 0618
 #pragma warning restore 0169
