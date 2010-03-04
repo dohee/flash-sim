5e3bdfc9024aa5546d1f7e121dd26b9bb4fe37b5
hiYoungCat <hiYoungCat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2010-03-04 10:46:48 +0000

    将 TrivalDevice 改名为 NullDevice.

--- Buffers/Devices/NullBlockDevice.cs
+++ Buffers/Devices/NullBlockDevice.cs
@@ -3,9 +3,9 @@ using Buffers.Devices;
 
 namespace Buffers.Devices
 {
-	public sealed class TrivalBlockDevice : BlockDeviceBase
+	public sealed class NullBlockDevice : BlockDeviceBase
 	{
-		public override string Name { get { return "TrivalDevice"; } }
+		public override string Name { get { return "NullDevice"; } }
 		public override uint PageSize { get { return 0; } protected set { } }
 		protected override void DoRead(uint pageid, byte[] result) { }
 		protected override void DoWrite(uint pageid, byte[] data) { }
