7a7b49428b8ee7ea4f8754e2f7b599a712ddf627
hiYoungCat <hiYoungCat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2010-03-27 03:40:56 +0000

    写了一点 FTL。实在不知道应该咋写了.....

--- trunk/Buffers/FTL/SimpleLogPageFTL.cs
+++ trunk/Buffers/FTL/SimpleLogPageFTL.cs
@@ -1,20 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
+using Buffers.Utilities;
+using Buffers.Devices;
+using System.Diagnostics;
 
 namespace Buffers.FTL
 {
 	public sealed class SimpleLogPageFTL : FTLBase
 	{
-		public SimpleLogPageFTL(IErasableDevice device)
-			: base(device) { }
+		private readonly uint logAreaSize;
+		private readonly uint logRelativeOffset;
+		private readonly Dictionary<BlockPageId, BlockPageId> forward;
+		private readonly Dictionary<BlockPageId, BlockPageId> backward;
+		private readonly Dictionary<uint, LogBlockInfo> logInfos;
+		private readonly SortedDictionary<LogBlockInfo, object> fullLogInfos;
+
+		public SimpleLogPageFTL(IErasableDevice device, uint nBlockOfLogArea)
+			: base(device)
+		{
+			if (nBlockOfLogArea == 0)
+				throw new ArgumentOutOfRangeException("nBlockOfLogArea", "cannot be zero");
+
+			logAreaSize = nBlockOfLogArea;
+		}
+
+		public override string Description
+		{
+			get
+			{
+				return Utils.FormatDesc("LogArea", logAreaSize);
+			}
+		}
+		public uint NBlockOfLogArea { get { return logAreaSize; } }
+
+		private BlockPageId ToLogBlockArea(BlockPageId relative)
+		{
+			Debug.Assert(relative.BlockId < logAreaSize);
+			return new BlockPageId(relative.BlockId + logRelativeOffset, relative.PageId);
+		}
+		private BlockPageId ToLogRelative(BlockPageId absolute)
+		{
+			Debug.Assert(absolute.BlockId >= logRelativeOffset);
+			uint relblockid = absolute.BlockId - logRelativeOffset;
+			Debug.Assert(relblockid < logAreaSize);
+			return new BlockPageId(relblockid, absolute.PageId);
+		}
 
 		protected override void DoRead(uint pageid, byte[] result)
 		{
-			dev.Read(pageid, result);
+			BlockPageId bpid = dev.ToBlockPageId(pageid);
+			BlockPageId logbpid;
+
+			if (forward.TryGetValue(bpid, out logbpid))
+				dev.Read(dev.ToUniversalPageId(ToLogBlockArea(logbpid)), result);
+			else			
+				dev.Read(pageid, result);
 		}
 		protected override void DoWrite(uint pageid, byte[] data)
 		{
 			dev.Write(pageid, data);
 		}
-		
+
+
+		private class LogBlockInfo : IComparable<LogBlockInfo>
+		{
+			public readonly uint BlockId;
+			public uint NPageUsed;
+			public readonly HashSet<uint> AssociatedBlocks;
+
+			public LogBlockInfo(uint blockId)
+			{
+				BlockId = blockId;
+				NPageUsed = 0;
+				AssociatedBlocks = new HashSet<uint>();
+			}
+
+			public int CompareTo(LogBlockInfo other)
+			{
+				return AssociatedBlocks.Count - other.AssociatedBlocks.Count;
+			}
+		}
+
 	}
 }
