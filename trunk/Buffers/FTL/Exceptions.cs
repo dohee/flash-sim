ef0660c0500a29d8c5bbfb3d532c25ecaae26510
hiyoungcat <hiyoungcat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2010-03-31 11:31:19 +0000

    已完成代码迁移工作。征测试....

---
+++ trunk/Buffers/FTL/Exceptions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Buffers.FTL
+{
+	[Serializable]
+	public class FTLException : ApplicationException
+	{
+		public FTLException() { }
+		public FTLException(string message) : base(message) { }
+		public FTLException(string message, Exception inner) : base(message, inner) { }
+		protected FTLException(
+		  SerializationInfo info,
+		  StreamingContext context)
+			: base(info, context) { }
+	}
+
+	[global::System.Serializable]
+	public class FlashBlockBrokenException : FTLException
+	{
+		public FlashBlockBrokenException() { }
+		public FlashBlockBrokenException(string message) : base(message) { }
+		public FlashBlockBrokenException(string message, Exception inner) : base(message, inner) { }
+		protected FlashBlockBrokenException(
+		  SerializationInfo info,
+		  StreamingContext context)
+			: base(info, context) { }
+	}
+
+	[global::System.Serializable]
+	public class FlashNoMemoryException : FTLException
+	{
+		public FlashNoMemoryException() { }
+		public FlashNoMemoryException(string message) : base(message) { }
+		public FlashNoMemoryException(string message, Exception inner) : base(message, inner) { }
+		protected FlashNoMemoryException(
+		  SerializationInfo info,
+		  StreamingContext context)
+			: base(info, context) { }
+	}
+
+	[global::System.Serializable]
+	public class FlashNotDirtyException : FTLException
+	{
+		public FlashNotDirtyException() { }
+		public FlashNotDirtyException(string message) : base(message) { }
+		public FlashNotDirtyException(string message, Exception inner) : base(message, inner) { }
+		protected FlashNotDirtyException(
+		  SerializationInfo info,
+		  StreamingContext context)
+			: base(info, context) { }
+	}
+
+	[global::System.Serializable]
+	public class InvalidLBAException : FTLException
+	{
+		public InvalidLBAException() { }
+		public InvalidLBAException(string message) : base(message) { }
+		public InvalidLBAException(string message, Exception inner) : base(message, inner) { }
+		protected InvalidLBAException(
+		  SerializationInfo info,
+		  StreamingContext context)
+			: base(info, context) { }
+	}
+
+	[global::System.Serializable]
+	public class InvalidPageStateException : FTLException
+	{
+		public InvalidPageStateException() { }
+		public InvalidPageStateException(string message) : base(message) { }
+		public InvalidPageStateException(string message, Exception inner) : base(message, inner) { }
+		protected InvalidPageStateException(
+		  SerializationInfo info,
+		  StreamingContext context)
+			: base(info, context) { }
+	}
+}
