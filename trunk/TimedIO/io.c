b25ee9996570ec2352f08ac7ef1aa515ae27a594
hiyoungcat <hiyoungcat@9895585c-9ddb-11de-bf4a-bd5be0e2b6d7>
2010-05-22 09:19:20 +0000

    better functionality. note: the commandline interface is changed!

--- trunk/TimedIO/io.c
+++ trunk/TimedIO/io.c
@@ -20,15 +20,16 @@ const int BUFSIZE = 1024*1024*128;
 const char *g_progname = NULL;
 char *g_buf = NULL;
 time_t g_startTime, g_endTime;
-int g_reqcount = 0, g_rewindcount = 0, g_childOutputFD = -1, g_stopnow = 0;
+int g_reqcount = 0, g_rewindcount = 0, g_childOutputFD = -1, g_phase = 0;
 FILE *g_statfile = NULL;
 
 
 void Usage()
 {
-	fprintf(stderr, "Usage: %s <File> read|write seq|rnd <TimePeriod> <ReqSize> [<ProcessCount>]\n"
-					"       %s <File> create <Size>\n",
-		g_progname, g_progname);
+	fprintf(stderr, "Usage: %s <File> create <Size>\n"
+					"       %s <File> read|write-sync|write-async seq|rnd\n"
+					"           <TimePeriod> <ReqSize> [<ProcessCount>]\n",
+		g_progname, g_progname, g_progname);
 
 	exit(1);
 }
@@ -101,23 +102,29 @@ long long ParseSize(const char *str)
 	return size;
 }
 int ParseArguments(int argc, char *argv[], int echo,
-	const char **filename, enum Actions *action, int *rand,
-	int *timeperiod, long long *size, int *nprocess)
+	const char **filename, enum Actions *action, int *sync, int *rand,
+	int *timeperiod, int *preperiod, int *postperiod, long long *size, int *nprocess)
 {
 	if (argc < 4)
 		return -1;
 
 	*filename = argv[1];
+	*sync = 0;
 	const char *actionstr = argv[2];
 
-	if (strcasecmp(actionstr, "read") == 0)
+	if (strcasecmp(actionstr, "read") == 0) {
 		*action = ActionRead;
-	else if (strcasecmp(actionstr, "write") == 0)
+	} else if (strcasecmp(actionstr, "write-sync") == 0) {
 		*action = ActionWrite;
-	else if (strcasecmp(actionstr, "create") == 0)
+		*sync = 1;
+	} else if (strcasecmp(actionstr, "write-async") == 0) {
+		*action = ActionWrite;
+		*sync = 0;
+	} else if (strcasecmp(actionstr, "create") == 0) {
 		*action = ActionCreate;
-	else
+	} else {
 		return -1;
+	}
 
 	char timestr[100]; time_t timet;
 	timet = time(NULL);
@@ -137,16 +144,24 @@ int ParseArguments(int argc, char *argv[], int echo,
 		else
 			return -1;
 
-		*timeperiod = atoi(argv[4]);
+		char periodstr[100], *endstr;
+		strcpy(periodstr, argv[4]);
+		strcat(periodstr, "+0+0");
+		*timeperiod = (int)strtol(periodstr, &endstr, 10);
+		*preperiod = (int)strtol(endstr+1, &endstr, 10);
+		*postperiod = (int)strtol(endstr+1, &endstr, 10);
+
 		*size = ParseSize(argv[5]);
 		*nprocess = (argc>=7 ? atoi(argv[6]) : 1);
 
 		if (echo)
-			printf("%s  %s  %s  %dsec  %lldB/req  %dprocesses  '%s'\n",
+			printf("%s  %s  %s  %s  %d+%d+%dsec  %lldB/req  %dprocesses  '%s'\n",
 				timestr,
 				(*action==ActionRead ? "Read":"Write"),
+				(*sync ? "Sync" : "Async"),
 				(*rand ? "Rnd" : "Seq"),
-				*timeperiod, *size, *nprocess, *filename);
+				*timeperiod, *preperiod, *postperiod,
+				*size, *nprocess, *filename);
 	}
 
 	if (echo)
@@ -179,19 +194,19 @@ int FileCreate(const char *filename, long long size)
 	return 0;
 }
 
-void ChildFileAccess(const char *filename, int isWrite, int random,
+void ChildFileAccess(const char *filename, int isWrite, int sync, int random,
 	long long reqsize, off_t curofs, off_t ofsmax)
 {
 	int r, fd=0;
 	off_t ofr;
 
-	if ((fd = open(filename, (isWrite?O_WRONLY:O_RDONLY) | O_NDELAY | O_SYNC)) < 0)
+	if ((fd = open(filename, (isWrite?O_WRONLY:O_RDONLY) | (sync?O_SYNC|O_NDELAY:0))) < 0)
 		Error(fd, "cannot open file");
 
 	if ((ofr = lseek(fd, curofs*reqsize, SEEK_SET)) == (off_t)-1)
 		Error((int)ofr, "lseek failed");
 
-	while (!g_stopnow) {
+	while (g_phase < 3) {
 		if (random) {
 			off_t offset = (rand() % ofsmax) * reqsize;
 			if ((ofr = lseek(fd, offset, SEEK_SET)) == (off_t)-1)
@@ -213,27 +228,42 @@ void ChildFileAccess(const char *filename, int isWrite, int random,
 				Error(r, "read failed");
 		}
 
-		//if ((r = fsync(fd)) < 0)
-		//	Error(r, "fsync failed");
-
 		++g_reqcount;
 	}
 }
 static void ChildAlarmHdlr(int signo)
 {
-	g_stopnow = 1;
+	g_phase++;
 }
 
-int FileAccess(const char *filename, int isWrite, int random,
-	int timeperiod, long long reqsize, int nprocess)
+void PrintDot(union sigval val)
 {
-	int result=0, r, i, fd=0, fdchilds[100]={0}, childinfo[2];
+	fprintf(stderr, ".");
+}
+
+int FileAccess(const char *filename, int isWrite, int sync, int random,
+	int timeperiod, int preperiod, int postperiod, long long reqsize, int nprocess)
+{
+	int result=0, r, i, j, fd=0, fdchilds[100]={0}, childinfo[2];
+	pid_t pidchilds[100] = {0};
 	struct stat statbuf;
+	struct sigevent sigev;
+	struct itimerspec tmrspec;
 	off_t ofsmax=0;
 
+	sigev.sigev_notify = SIGEV_THREAD;
+	sigev.sigev_notify_function = PrintDot;
+	sigev.sigev_notify_attributes = NULL;
+	timer_t tmrid;
+	tmrspec.it_interval.tv_sec = 2;
+	tmrspec.it_interval.tv_nsec = 0;
+	tmrspec.it_value.tv_sec = 0;
+	tmrspec.it_value.tv_nsec = 0;
+
+	if ((r = timer_create(CLOCK_REALTIME, &sigev, &tmrid)) < 0)
+		Error(r, "timer_create failed");
 	if ((r = access(filename, (isWrite ? W_OK : R_OK))) < 0)
 		Error(r, "access failed");
-
 	if ((r = stat(filename, &statbuf)) < 0)
 		Error(r, "stat failed");
 
@@ -257,15 +287,31 @@ int FileAccess(const char *filename, int isWrite, int random,
 			close(pipes[0]);
 			g_childOutputFD = pipes[1];
 			signal(SIGALRM, ChildAlarmHdlr);
-			alarm(timeperiod);
-			ChildFileAccess(filename, isWrite, random, reqsize, ofsmax*i/nprocess, ofsmax);
+			ChildFileAccess(filename, isWrite, sync, random, reqsize, ofsmax*i/nprocess, ofsmax);
 			exit(0);
 		} else { /* we are the parent */
 			close(pipes[1]);
 			fdchilds[i] = pipes[0];
+			pidchilds[i] = pid;
 		}
 	}
 
+
+	if ((r = timer_settime(tmrid, 0, &tmrspec, NULL)) < 0)
+		Error(r, "timer_settime failed");
+
+	int sleeptime[3] = {preperiod, timeperiod, postperiod};
+
+	for (j=0; j<3; ++j) {
+		sleep(sleeptime[j]);
+		fprintf(stderr, "|");
+
+		for (i=0; i<nprocess; ++i)
+			if ((r = kill(pidchilds[i], SIGALRM)) < 0)
+				Error(r, "sending SIGALRM to Child.%d (pid=%d) failed", i, pidchilds[i]);
+	}
+
+	fprintf(stderr, "\n");
 	printf("Children's ReqCount/RewindCount: ");
 
 	for (i=0; i<nprocess; ++i) {
@@ -280,6 +326,7 @@ int FileAccess(const char *filename, int isWrite, int random,
 	}
 
 	printf("\n");
+	timer_delete(tmrid);
 	g_endTime = time(NULL);
 	return 0;
 }
@@ -292,19 +339,19 @@ int main(int argc, char *argv[])
 	int r;
 	const char *filename;
 	enum Actions action;
-	int rand, timeperiod, nprocess;
+	int sync, rand, time1, time2, time3, nprocess;
 	long long size;
 
 	if ((r = ParseArguments(argc, argv, 1,
-		&filename, &action, &rand, &timeperiod, &size, &nprocess)) < 0)
+		&filename, &action, &sync, &rand, &time2, &time1, &time3, &size, &nprocess)) < 0)
 		Usage();
 
 	if (action == ActionCreate) {
 		return FileCreate(filename, size);
 	} else if (action == ActionRead) {
-		return FileAccess(filename, 0, rand, timeperiod, size, nprocess);
+		return FileAccess(filename, 0, sync, rand, time2, time1, time3, size, nprocess);
 	} else if (action == ActionWrite) {
-		return FileAccess(filename, 1, rand, timeperiod, size, nprocess);
+		return FileAccess(filename, 1, sync, rand, time2, time1, time3, size, nprocess);
 	} else {
 		Usage();
 	}
